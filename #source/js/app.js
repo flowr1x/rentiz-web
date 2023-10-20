8f29b18a99a7a7f0167804471daf2e54861455ea
Olegeg <bgpfnic@protonmail.com>
2023-10-21 01:28:25 +0300

    added a section search

--- #source/js/app.js
+++ #source/js/app.js
@@ -1,3 +1,4 @@
 "use strict"
 
 // @prepros-append menuBurger.js
+// @prepros-append dropdown.js
---
+++ #source/js/dropdown.js
@@ -0,0 +1,14 @@
+const filter = document.querySelector(".filter");
+if (filter) {
+    const blockFilter = filter.querySelectorAll(".block-filter");
+    blockFilter.forEach(item => {
+        item.addEventListener("click", event => {
+            item.querySelector(".block-filter__dropdown").classList.toggle("_active");
+            item.querySelector(".block-filter__icon").classList.toggle("_active");
+
+            if (event.target.classList.contains("block-filter__item")) {
+                item.querySelector(".block-filter__value").textContent = event.target.textContent;
+            }
+        })
+    })
+}
