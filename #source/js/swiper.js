f55663c647aa6f8f5f30042fee8710fdf978bfc2
Olegeg <bgpfnic@protonmail.com>
2023-10-22 14:24:21 +0300

    fix slider

--- #source/js/swiper.js
+++ #source/js/swiper.js
@@ -1,9 +1,17 @@
 const swiper = new Swiper('.swiper', {
     spaceBetween: 20,
-    slidesPerView: 3,
+    slidesPerView: 1,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      992: {
+        slidesPerView: 3,
+      },
+      767: {
+        slidesPerView: 2,
+      },
+    }
   });
   
